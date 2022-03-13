2582b8795a59a04b2cb1edff81af48dfe69f9b16
AbinMuds <abinashmudbhari@gmail.com>
2022-03-13 12:56:28 -0600

    adds components for Header and setup Header with a image

--- food-order/src/App.js
+++ food-order/src/App.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, {Fragment} from "react";
+import Header from "./components/Layout/Header";
 
 function App() {
   return (
-    <div className="App">
-      <h1>The LOader Page</h1>
-    </div>
+    <Fragment>
+      <Header/>
+    </Fragment>
   );
 }
 
--- food-order/src/components/Layout/Header.js
+++ food-order/src/components/Layout/Header.js
@@ -1,6 +1,19 @@
+import { Fragment } from 'react';
+
+import HeaderCartButton from './HeaderCartButton';
+import mealsImage  from '../../assets/meals.jpg';
+import classes from "./Header.module.css";
 
 const Header = props => {
-    return <h1>This is Header</h1>
+    return <Fragment>
+        <header className={classes.header}>
+            <h1>ReactMeals</h1>
+            <HeaderCartButton />
+        </header>
+        <div className={classes['main-image']}>
+            <img src={mealsImage} alt="A table full of delicious food"/>
+        </div>
+    </Fragment>
 }
 
 export default Header;
